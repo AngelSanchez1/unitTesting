b2a7d47ca298d0e8ef578171526e78e144d90d73
Angel Sanchez <dual.angel.sanchez@gmail.com>
2022-06-15 16:45:19 -0500

    se creo el primer set de pruebas con jasmine

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,21 @@
-const saludar = require('../app');
+const saludar = require("../app");
 
-it('La función saluda',()=>{
-    expect(saludar('Angel')).toBe('Hola Angel');
+var x = false;
+var a = {};
+var b = {};
+
+describe("Verificar que la variable es true", () => {
+  it("La función saluda", () => {
+    expect(saludar("Angel")).toContain("Hola Angel");
+  });
+
+  it('X es true', ()=>{
+    expect(x).toBe(false);
+    expect(x).toEqual(false);
+  } );
+
+  it('objetos iguales', ()=>{
+    expect(a).toEqual(b);
+  })
+
 });
-
